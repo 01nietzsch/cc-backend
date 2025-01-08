a91e73ae179dcc107a35b3212edff4a120d5d936
01nietzsch <vladimirjurien@gmail.com>
2025-01-08 14:38:56 +0000

    insh

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import os
 
 # Load the pre-trained models
 MODEL1_PATH = "yield_strength_regressor.pkl"
@@ -49,4 +50,4 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
